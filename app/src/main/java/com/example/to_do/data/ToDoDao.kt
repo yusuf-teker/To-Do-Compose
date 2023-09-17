849d0196d4df495fa67c9ecaa25d6b75f86ea7fe
Yusuf <79746018+yusuf-teker@users.noreply.github.com>
2023-09-17 19:15:02 +0300

    Dao created.

---
+++ app/src/main/java/com/example/to_do/data/ToDoDao.kt
@@ -0,0 +1,43 @@
+package com.example.to_do.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.to_do.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER by id ASC")
+    fun getAllTasks(): Flow<List<ToDoTask>> //no need for suspend keyword cause we used flow which running asynchronously by default
+
+    @Query("SELECT * FROM todo_table WHERE id = :taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query ("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END ")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END ")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
+
+
+}
---
+++ app/src/main/java/com/example/to_do/data/ToDoDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.to_do.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.to_do.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+}
