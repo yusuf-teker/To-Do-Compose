dbb21e8b46515c3e425803d14add0e3d4493b768
Yusuf <79746018+yusuf-teker@users.noreply.github.com>
2023-09-17 23:31:09 +0300

    Dagger-Hilt setted up

---
+++ app/src/main/java/com/example/to_do/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.to_do.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.to_do.data.ToDoDatabase
+import com.example.to_do.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)//Sigelton cause we use same database whenever we need
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context = context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+
+}
