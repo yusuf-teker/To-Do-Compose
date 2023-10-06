a2c0867bf951976d65f6f12e12787e6a2c1ebea5
Aykut Sever <aykut.sever@tebarf.com.tr>
2023-10-06 17:32:00 +0300

    Add status bar color.

--- app/src/main/java/com/example/to_do/MainActivity.kt
+++ app/src/main/java/com/example/to_do/MainActivity.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.to_do.navigation.SetupNavigation
 import com.example.to_do.ui.theme.ToDoTheme
+import com.example.to_do.ui.theme.statusBarColor
 import com.example.to_do.ui.viewmodels.SharedViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,9 +28,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
+
             ToDoTheme {
 
                 navController = rememberNavController()
+
+                StatusBarColor(statusBarColor = MaterialTheme.colorScheme.statusBarColor)
 
                 SetupNavigation(
                     navController = navController,
@@ -31,6 +41,20 @@ class MainActivity : ComponentActivity() {
                 )
 
             }
+        }
+    }
+
+    @Composable
+    private fun StatusBarColor(statusBarColor: Color) {
+        val systemUiController = rememberSystemUiController()
+        val useDarkIcons = !isSystemInDarkTheme()
+
+        DisposableEffect(systemUiController, useDarkIcons) {
+            systemUiController.setStatusBarColor(
+                color = statusBarColor,
+                darkIcons = useDarkIcons
+            )
+            onDispose {}
         }
     }
 }
--- app/src/main/java/com/example/to_do/ui/theme/Theme.kt
+++ app/src/main/java/com/example/to_do/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -53,14 +54,15 @@ fun ToDoTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
+    /*val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
-    }
+    }*/
+
 
     MaterialTheme(
         colorScheme = colorScheme,
